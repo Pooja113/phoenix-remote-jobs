052325f69524462e55e756781d862b6e5bef31c0
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-05-11 19:04:25 +0530

    Update JobList.js

--- frontend/src/components/JobsList/JobList.js
+++ frontend/src/components/JobsList/JobList.js
@@ -1,34 +1,25 @@
 import React, { useEffect } from 'react'
 import './JobList.css'
-import JobItem from './JobItem'
-import { useStateValue } from '../../StateProvider';
-import * as api from '../../api'
-import { FETCH_ALL_JOBS } from "../../actionTypes";
+//import JobItem from './JobItem'
+// import { useStateValue } from '../../StateProvider';
+// import * as api from '../../api'
+// import { FETCH_ALL_JOBS } from "../../actionTypes";
 
 
 const JobList = () => {
-  const [{posts},dispatch] = useStateValue();
-  useEffect( ()=>{
-    api.fetchPosts().then((res)=>{
-      dispatch({
-      type:FETCH_ALL_JOBS,
-      payload: {posts : res}
-    });
-    })
-    .catch((err)=>console.log(err))
+//   const [{posts},dispatch] = useStateValue();
+//   useEffect( ()=>{
+//     api.fetchPosts().then((res)=>{
+//       dispatch({
+//       type:FETCH_ALL_JOBS,
+//       payload: {posts : res}
+//     });
+//     })
+//     .catch((err)=>console.log(err))
 
-  },[dispatch])
+//   },[dispatch])
   return (
-    <section id="jobs-container">
-    <h1 className="head-primary">Programming Jobs</h1>
-      { !posts.data ? "No Jobs" :
-          posts.data.map((job)=>{
-              return (<div key={job._id} className="jobs">
-                  <JobItem job={job} />
-                </div>)
-            })
-        }
-  </section>
+    <h1>test</h1>
   )
 }
 
